570c983078208061e14ac8cc69734f8f53ed7e34
Yang Xin <yangxin@163.com>
2019-11-19 16:28:24 +0800

    'init'

---
+++ src/main/java/com/sunny/jib/SunnyJibApplication.java
@@ -0,0 +1,13 @@
+package com.sunny.jib;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SunnyJibApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SunnyJibApplication.class, args);
+    }
+
+}
