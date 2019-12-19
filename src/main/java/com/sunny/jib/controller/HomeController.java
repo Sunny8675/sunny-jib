f3bc40d64f6effc81da43c84cb20c9c9b30d6666
Yang Xin <yangxin@163.com>
2019-12-19 14:26:52 +0800

    update

--- src/main/java/com/sunny/jib/controller/HomeController.java
+++ src/main/java/com/sunny/jib/controller/HomeController.java
@@ -1,6 +1,7 @@
 package com.sunny.jib.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDateTime;
@@ -8,12 +9,12 @@ import java.time.LocalDateTime;
 @RestController
 public class HomeController {
 
-    @RequestMapping("/home")
-    public String home() {
-        return "Hello World";
+    @GetMapping("/home/{name}")
+    public String home(@PathVariable String name) {
+        return "Hello：" + name;
     }
 
-    @RequestMapping("/index")
+    @GetMapping("/index")
     public LocalDateTime index() {
         return LocalDateTime.now();
     }
